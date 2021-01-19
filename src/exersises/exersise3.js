31213b260f01407bcbb2ad9ac462d95af06e4366
imansedky5 <imans.ewaiti@gmail.com>
2021-01-19 19:14:14 +0200

    format profile

--- src/exersises/exersise3.js
+++ src/exersises/exersise3.js
@@ -1,25 +1,26 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 const Giphy = () => {
-    const [inputVal, setVal] = useState("");
+  const [inputVal, setVal] = useState("");
 
-    React.useEffect(() => {
-        const controller = new AbortController();
-        const {signal} = controller;
-        fetch(`https://api.giphy.com/v1/gifs/search?q=${inputVal}&limit=10&api_key=jsSguBk7KlRE7Fpiclxzzt70adW0nHt4`, {signal})
-        .then(res => res.json())
-        .then(({data}) => console.log(data))
-        .catch(err => console.log(err));
+  React.useEffect(() => {
+    const controller = new AbortController();
+    const { signal } = controller;
+    fetch(
+      `https://api.giphy.com/v1/gifs/search?q=${inputVal}&limit=10&api_key=jsSguBk7KlRE7Fpiclxzzt70adW0nHt4`,
+      { signal }
+    )
+      .then((res) => res.json())
+      .then(({ data }) => console.log(data))
+      .catch((err) => console.log(err));
 
-        return () => {
-            controller.abort()
+    return () => {
+      controller.abort();
+    };
+  }, [inputVal]);
 
-        }
-    }, [inputVal]);
+  return (
+    <input type="search" onChange={({ target: { value } }) => setVal(value)} />
+  );
+};
 
-           return (
-               <input type="search"  onChange = {({target:{value}}) => setVal(value)}/> 
-     );
-     
-}
- 
 export default Giphy;
